28cad65aab5caecabc4e414f1f39fc7e0645fc99
Aleksander Vostryakov <sanyok.10rus@gmail.com>
2021-10-26 07:02:42 +0300

    project ArrayList for Homework

---
+++ ArrayList/EntryPoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MyArrayList;
+
+namespace EntryPoint
+{
+    class EntryPoint
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList(new int[] { 7, 6, 2, 4, 1 });
+            //Console.WriteLine(arr.Length);
+            arr.AddFirst(new ArrayList(new int[] { 3, 3, 3, 999 }));
+            arr.Sort();
+            arr.Print();
+            ////arr.AddLast(1);
+            ////arr.AddLast(2);
+            //arr.Print();
+            //Console.ReadKey();
+            //arr.AddFirst(3);
+            //arr.Print();
+            //arr.AddAt(2, 4);
+            //arr.Print();
+            //arr.RemoveLast();
+            //arr.Print();
+            //arr.RemoveFirst();
+            //arr.Print();
+            //Console.ReadKey();
+            Type t = arr.GetType();
+            foreach (var method in t.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+
+        }
+    }
+}
